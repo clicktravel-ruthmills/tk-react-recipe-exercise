41af0b6d69d725c80b3da1a8a86d86d49e8ca2c0
Ruth Mills <ruth.mills@clicktravel.com>
2021-08-26 16:27:16 +0100

    Get WIP routing working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+
+import ListRecipes from './screens/ListRecipes/ListRecipes';
+import CreateRecipe from './screens/CreateRecipe/CreateRecipe';
 
 import './App.css';
 
@@ -7,6 +10,26 @@ function App() {
   return (
     <div className="App">
       <Router>
+          <div>
+              <nav>
+                  <ul>
+                      <li>
+                          <Link to="/">List Recipes</Link>
+                      </li>
+                      <li>
+                          <Link to="/create">Create Recipe</Link>
+                      </li>
+                  </ul>
+              </nav>
+          </div>
+          <Switch>
+              <Route path="/create">
+                  <CreateRecipe />
+              </Route>
+              <Route path="/">
+                  <ListRecipes />
+              </Route>
+          </Switch>
       </Router>
     </div>
   );
--- src/screens/CreateRecipe/CreateRecipe.tsx
+++ src/screens/CreateRecipe/CreateRecipe.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 type Props = {
 };
 
-const ListRecipes = ({
+const CreateRecipe = ({
 }: Props) => {
 
     return (
         <div>
-            <p>LIST RECIPES WILL GO HERE!</p>
+            <p>CREATE RECIPE WILL GO HERE!</p>
         </div>
     );
 };
 
-export default ListRecipes;
+export default CreateRecipe;
