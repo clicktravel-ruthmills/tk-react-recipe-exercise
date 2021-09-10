48d10f0b2686351bafec10604a77ae856a7c606d
Ruth Mills <ruth.mills@clicktravel.com>
2021-09-10 14:09:07 +0100

    Get rid of unused component

--- src/components/IngredientForm/IngredientForm.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { ingredient as ingredientFabricator } from '../../data/fabricators';
-
-describe('<IngredientForm />', () => {
-    it('should render ingredient form', () => {
-
-    });
-});
