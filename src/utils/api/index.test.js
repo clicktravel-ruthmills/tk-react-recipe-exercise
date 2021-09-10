569bdb65f0330d3cb9aee525c4700f31fd8b5c82
Ruth Mills <ruth.mills@clicktravel.com>
2021-09-10 14:02:29 +0100

    Fix API tests

--- src/utils/api/index.test.js
+++ src/utils/api/index.test.js
@@ -11,35 +11,18 @@ const getMockFetch = (responseData, options = {}) => {
     return fetch;
 };
 
-test('request with no name search', async () => {
+test('GET request with no ID or name search', async () => {
   const mockFetch = getMockFetch();
-  const method = 'POST';
+  const method = 'GET';
   const id = undefined;
   const name = undefined;
+  const payload = undefined;
 
-  await request(method, id, name);
+  await request(method, id, name, payload);
 
   expect(mockFetch).toHaveBeenCalledTimes(1);
   expect(mockFetch).toHaveBeenCalledWith(
     '/recipes/',
-    {
-      headers: { 'Content-Type': 'application/json' },
-      method: 'POST',
-    },
-  );
-});
-
-test('request with ID', async () => {
-  const mockFetch = getMockFetch();
-  const method = 'GET';
-  const id = 123;
-  const name = undefined;
-
-  await request(method, id, name);
-
-  expect(mockFetch).toHaveBeenCalledTimes(1);
-  expect(mockFetch).toHaveBeenCalledWith(
-    '/recipes/123',
     {
       headers: { 'Content-Type': 'application/json' },
       method: 'GET',
@@ -47,11 +30,71 @@ test('request with ID', async () => {
   );
 });
 
-test('request with name search', async () => {
+test('POST request with payload but no ID', async () => {
+  const mockFetch = getMockFetch();
+  const method = 'POST';
+  const id = undefined;
+  const name = undefined;
+  const payload = { name: 'test name', description: 'test description'};
+
+  await request(method, id, name, payload);
+
+  expect(mockFetch).toHaveBeenCalledTimes(1);
+  expect(mockFetch).toHaveBeenCalledWith(
+    '/recipes/',
+    {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: '{"name":"test name","description":"test description"}',
+    },
+  );
+});
+
+test('POST request with payload and ID', async () => {
+  const mockFetch = getMockFetch();
+  const method = 'POST';
+  const id = 123;
+  const name = undefined;
+  const payload = { name: 'test name', description: 'test description'};
+
+  await request(method, id, name, payload);
+
+  expect(mockFetch).toHaveBeenCalledTimes(1);
+  expect(mockFetch).toHaveBeenCalledWith(
+    '/recipes/123/',
+    {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: '{"name":"test name","description":"test description"}',
+    },
+  );
+});
+
+test('GET request with ID', async () => {
+  const mockFetch = getMockFetch();
+  const method = 'GET';
+  const id = 123;
+  const name = undefined;
+  const payload = undefined;
+
+  await request(method, id, name, payload);
+
+  expect(mockFetch).toHaveBeenCalledTimes(1);
+  expect(mockFetch).toHaveBeenCalledWith(
+    '/recipes/123/',
+    {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'GET',
+    },
+  );
+});
+
+test('GET request with name search', async () => {
   const mockFetch = getMockFetch();
   const method = 'GET';
   const id = undefined;
   const name = 'Pi';
+  const payload = undefined;
 
   await request(method, id, name);
 
